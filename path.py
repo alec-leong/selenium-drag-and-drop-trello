ac1b724d620a332db2883341f41bbf473da5dec6
Alec Leong <leongalec@gmail.com>
2021-03-04 20:55:06 -0800

    Determine the relative path to the appropiate chromedriver.exe based on the user's operating system.

---
+++ path.py
@@ -0,0 +1,17 @@
+import platform
+import re
+
+operating_system = platform.system()
+
+if operating_system == 'Linux':
+    chromedriver_rel_path = 'chromedrivers/chromedriver_linux64/'
+elif operating_system == 'Windows':
+    chromedriver_rel_path = 'chromedrivers/chromedriver_win32/'
+elif operating_system == 'Darwin':
+    chromedriver_rel_path = 'chromedrivers/chromedriver_mac64_m1/' \
+        if re.search('ARM', platform.system()) \
+        else 'chromedrivers/chromedriver_mac64/'
+else:
+    raise OSError(f'Unsupported operating system.\n\tActual: {operating_system}\n\tExpected: Linux, Windows, or Darwin')
+
+chromedriver_rel_path += 'chromedriver.exe'
